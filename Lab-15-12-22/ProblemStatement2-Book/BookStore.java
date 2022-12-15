ccfe17300dbd3b7b85cc942cbe75dcdd5aa32043
SOURAV DAS <100298703+souravbarisha@users.noreply.github.com>
2022-12-15 15:35:55 +0530

    Add files via upload

---
+++ Lab-15-12-22/ProblemStatement2-Book/BookStore.java
@@ -0,0 +1,44 @@
+package com.book;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class BookStore {
+	
+	ArrayList<Book>al1=new ArrayList<Book>();
+	public void addBook(Book b) {
+		al1.add(b);
+	}
+	//Search book based on title
+
+	public void searchByTitle(String title) {
+		Iterator<Book> it=al1.iterator();
+		while(it.hasNext()) {
+			Book obj=(Book)it.next();
+			if(obj.title==title) {
+				System.out.println("Book found=");
+				System.out.println(obj.title+" "+obj.author);
+			}
+		}
+	}
+	//Search book based on author
+	public void searchByAuthor(String auth) {
+		Iterator<Book> it=al1.iterator();
+		while(it.hasNext()) {
+			Book obj=(Book)it.next();
+			if(obj.author==auth) {
+				System.out.println("Book found=");
+				System.out.println(obj.title+" "+obj.author);
+			}
+		}
+	}
+	public void display() {
+		Iterator<Book> it=al1.iterator();
+		while(it.hasNext()) {
+			Book obj=(Book)it.next();
+			System.out.println(obj.bookID+" "+obj.title+" "+obj.author);
+
+		}
+	}
+	
+}
